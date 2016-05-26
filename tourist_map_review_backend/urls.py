202e5aca099cf242d4d557ce59e0732ced94e75d
SwitchUA <web.switch.ua@gmail.com>
2016-05-26 23:46:31 +0300

    Media urls fixeed

--- tourist_map_review_backend/urls.py
+++ tourist_map_review_backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from apis.urls import router
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,4 +25,5 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
